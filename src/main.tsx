0afb44e0cb317321d485cc83030aa8d42115d203
MishraShardendu22 <shardendumishrabrave@gmail.com>
2025-08-17 23:43:53 +0530

    Signed-off-by: Shardendu Mishra <shardendumishrabrave@gmail.com>

---
+++ src/main.tsx
@@ -0,0 +1,16 @@
+import './index.css'
+import App from './App.tsx'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { registerSW } from 'virtual:pwa-register'
+
+registerSW({
+  onNeedRefresh() {},
+  onOfflineReady() {},
+})
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
